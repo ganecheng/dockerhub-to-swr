6af083b654c2691c744a1fe32d9e4e42160b695f
Gane_Cheng <1774465+gane_cheng@user.noreply.gitee.com>
2024-07-25 00:20:54 +0800

    for循环同步一个删除一个

--- sync-image.sh
+++ sync-image.sh
@@ -3,27 +3,10 @@
 # -e当命令发生错误的时候, 停止脚本的执行; -x把将要运行的命令用一个 + 标记之后显示出来
 set -ex
 
-# docker pull
-# 如果需要特定架构镜像可以手动指定  --platform linux/arm64 , linux/amd64 , linux/arm/v7 等信息
-
-# 不指定 cpu 架构
-cat trigger.txt | awk '{print "docker pull " $1} '
-cat trigger.txt | awk '{print "docker pull " $1} ' | sh
-
-#指定 cpu 架构
-# cat trigger.txt | awk '{print "docker pull --platform linux/arm64 " $1} '
-# cat trigger.txt | awk '{print "docker pull --platform linux/arm64 " $1} '| sh
-
-# docker tag
-cat trigger.txt | awk '{print "docker tag " $1 " swr.cn-south-1.myhuaweicloud.com/gsc-hub/" $1} '
-cat trigger.txt | awk '{print "docker tag " $1 " swr.cn-south-1.myhuaweicloud.com/gsc-hub/" $1} ' | sh
-
-# docker push
-cat trigger.txt | awk '{print "docker push swr.cn-south-1.myhuaweicloud.com/gsc-hub/" $1} '
-cat trigger.txt | awk '{print "docker push swr.cn-south-1.myhuaweicloud.com/gsc-hub/" $1} ' | sh
-
-# docker rmi
-cat trigger.txt | awk '{print "docker rmi " $1} '
-cat trigger.txt | awk '{print "docker rmi " $1} ' | sh
-cat trigger.txt | awk '{print "docker rmi swr.cn-south-1.myhuaweicloud.com/gsc-hub/" $1} '
-cat trigger.txt | awk '{print "docker rmi swr.cn-south-1.myhuaweicloud.com/gsc-hub/" $1} ' | sh
+list="trigger.txt"
+for i in $(cat ${list}); do
+    docker pull ${i}
+    docker tag ${i} swr.cn-south-1.myhuaweicloud.com/gsc-hub/${i}
+    docker push swr.cn-south-1.myhuaweicloud.com/gsc-hub/${i}
+    docker rmi ${i} swr.cn-south-1.myhuaweicloud.com/gsc-hub/${i}
+done
