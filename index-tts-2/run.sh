2fbd63a86d2ceb51aff672dbd5e831602c8d6b12
ganecheng <891731900@qq.com>
2025-10-22 18:23:44 +0800

    Update run.sh

--- index-tts-2/run.sh
+++ index-tts-2/run.sh
@@ -3,10 +3,14 @@
 # -e当命令发生错误的时候, 停止脚本的执行;
 set -ex
 
+cd /app
+
 source /root/.profile
+
+source .venv/bin/activate
 
 python -V
 
-uv run tools/gpu_check.py
+python tools/gpu_check.py
 
-uv run webui.py --host 0.0.0.0 --port 7860 --fp16 --cuda_kernel
+python webui.py --host 0.0.0.0 --port 7860 --fp16 --cuda_kernel
