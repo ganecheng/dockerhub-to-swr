6c58507080068afec815f1341e0f99d27e887570
Gane_Cheng <1774465+gane_cheng@user.noreply.gitee.com>
2025-11-11 20:57:32 +0800

    sensevoice镜像制作

--- sensevoice/openeuler/os_clean.sh
+++ sensevoice/openeuler/os_clean.sh
@@ -4,7 +4,6 @@
 set -ex
 
 # 清理无用文件
-uv cache clean
 rm -rf /app/checkpoints/.cache /root/.cache/huggingface/xet /opt/conda
 yum clean all && rm -rf /var/cache/* /var/log/* /var/tmp/* /tmp/*
 yum -y autoremove git git-lfs
