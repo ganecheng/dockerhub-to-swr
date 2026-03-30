84214fc0f6186feb33ecf45df2302a8672b7a2a0
Gane_Cheng <1774465+gane_cheng@user.noreply.gitee.com>
2026-03-30 15:32:57 +0800

    pytorch镜像制作

--- pytorch/install_pytorch.sh
+++ pytorch/install_pytorch.sh
@@ -3,7 +3,11 @@
 # -e当命令发生错误的时候, 停止脚本的执行;
 set -ex
 
-pip install --no-cache-dir torch>=2.11.0,<2.12.0 torchvision>=0.26.0,<0.27.0 torchaudio>=2.11.0,<2.12.0 --index-url https://download.pytorch.org/whl/cu130
+pip install --no-cache-dir \
+    torch==2.11.* \
+    torchvision==0.26.* \
+    torchaudio==2.11.* \
+    --index-url https://download.pytorch.org/whl/cu130
 
 # 清理无用文件
 sh /os_clean.sh
