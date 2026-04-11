dfde365a24ea4db3aedf121959b83c186ee6b9cf
github-desktop <github-desktop@user.noreply.github.com>
2026-04-11 13:05:07 +0800

    ace-step镜像制作

--- ace-step/code.sh
+++ ace-step/code.sh
@@ -12,7 +12,7 @@ git clone https://github.com/ACE-Step/ACE-Step-1.5.git . && git checkout 82252c2
 rm -rf .git
 
 # 安装依赖
-pip install --no-cache-dir -r requirements.txt
+uv sync
 
 # 清理无用文件
 sh /os_clean.sh
--- ace-step/ubuntu/os_init.sh
+++ ace-step/ubuntu/os_init.sh
@@ -8,6 +8,35 @@ chmod 777 /run.sh
 
 # 应用目录
 mkdir -pv /app
+cd /app
+
+# 软件安装
+apt-get update && apt-get install -y git git-lfs net-tools tree curl wget python3 python3-pip ffmpeg
+
+# 设置全局python命令
+ln -s /usr/bin/python3 /usr/bin/python
+
+# 使用uv安装依赖
+pip install --no-cache-dir -U uv
+# 模型工具
+pip install --no-cache-dir "huggingface-hub[cli,hf_xet]"
+pip install --no-cache-dir modelscope
+
+# 常用命令自定义
+cat >>/etc/bash.bashrc <<'EOF'
+
+# 输入..并回车，即可返回到上一级目录
+shopt -s autocd
+
+reset_alias() {
+    # 命名别名配置
+    alias l="ls -alh"
+    alias ll="ls -alh"
+    alias lll="ls -alh"
+}
+PROMPT_COMMAND="reset_alias;$PROMPT_COMMAND"
+
+EOF
 
 # 清理无用文件
 sh /os_clean.sh
