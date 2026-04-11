18cdce23f89938e8431c2d9d2efeee54a52e6776
github-desktop <github-desktop@user.noreply.github.com>
2026-04-11 11:45:27 +0800

    ace-step镜像制作

--- ace-step/code.sh
+++ ace-step/code.sh
@@ -12,7 +12,7 @@ git clone https://github.com/ACE-Step/ACE-Step-1.5.git . && git checkout 82252c2
 rm -rf .git
 
 # 安装依赖
-uv sync
+pip install --no-cache-dir -r requirements.txt
 
 # 清理无用文件
 sh /os_clean.sh
