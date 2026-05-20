68158e76e88c3c9a49f8451ecdeaf543694cec73
ganecheng <891731900@qq.com>
2026-05-20 16:50:12 +0800

    Update install_pytorch.sh

--- pytorch/install_pytorch.sh
+++ pytorch/install_pytorch.sh
@@ -10,7 +10,7 @@ pip install --no-cache-dir \
     --index-url https://download.pytorch.org/whl/cu132
 
 pip install --no-cache-dir \
-    torchaudio==2.11.*
+    torchaudio==2.11.* \
     --index-url https://download.pytorch.org/whl/cu130
 
 # 清理无用文件
