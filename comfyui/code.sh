af9eeaad550d1604d2e0138f6a92e7913e1c59e9
ganecheng <891731900@qq.com>
2026-06-02 15:38:32 +0800

    Update code.sh

--- comfyui/code.sh
+++ comfyui/code.sh
@@ -8,7 +8,7 @@ mkdir -pv /default_comfyui_bundle/ComfyUI
 cd /default_comfyui_bundle/ComfyUI
 
 # 下载代码
-git clone https://github.com/Comfy-Org/ComfyUI.git . && git checkout a8d2519
+git clone https://github.com/Comfy-Org/ComfyUI.git . && git checkout a88e02b
 rm -rf .git
 
 # 安装依赖
