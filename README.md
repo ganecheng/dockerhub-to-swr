1b3f2513980f8c00153d8c4a5f5b77d785520f5d
g-ai-126 <269969053+g-ai-126@users.noreply.github.com>
2026-06-20 22:38:07 +0800

    refactor: 重构项目结构，升级CI运行环境
    - 将 sync-image.sh 迁移至 batch_sync_image/ 并重命名为 batch-sync-image.sh

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ## 使用方式
 
-提交需要同步的镜像到`trigger.txt`, 每一行一个镜像
+提交需要同步的镜像到`batch_sync_image_list.txt`, 每一行一个镜像
--- batch_sync_image/batch-sync-image.sh
+++ batch_sync_image/batch-sync-image.sh
@@ -3,7 +3,7 @@
 # -e当命令发生错误的时候, 停止脚本的执行; -x把将要运行的命令用一个 + 标记之后显示出来
 set -ex
 
-list="trigger.txt"
+list="batch_sync_image_list.txt"
 for i in $(cat ${list}); do
     docker pull ${i}
     docker tag ${i} swr.cn-southwest-2.myhuaweicloud.com/gsc-hub/${i}
