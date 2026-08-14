1e4787754d68cd36c1ef5e71d2e458e2aea1a547
g-ai-001 <g-ai-002@ganecheng.tech>
2026-08-14 14:42:44 +0800

    feat(comfyui): 升级 ComfyUI 版本至 v0.33.1

--- comfyui/code.sh
+++ comfyui/code.sh
@@ -9,7 +9,7 @@ cd /default_comfyui_bundle/ComfyUI
 
 # 下载代码
 git clone --filter=blob:none --no-checkout https://github.com/Comfy-Org/ComfyUI.git .
-git checkout v0.32.0
+git checkout v0.33.1
 rm -rf .git
 
 # 安装依赖
